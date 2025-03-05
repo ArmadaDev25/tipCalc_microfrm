74a783a384aff358ac311edd030aa73ecc93cb22
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2025-03-04 19:44:03 -0700

    Program now calculates the final bill amount

--- TipCalculator/TipCalculator/Form1.cs
+++ TipCalculator/TipCalculator/Form1.cs
@@ -11,16 +11,19 @@ namespace TipCalculator
         private bool areInputsValid(string inputBA, string inputTA) {
             double checkedBA = 0;
             double checkedTA = 0;
+            // Checks to make sure that the Inputed bill ammount is a number
             if (double.TryParse(inputBA, out checkedBA))
             {
+                // Checks to make sure the inputed tip amount is a number
                 if (double.TryParse(inputTA, out checkedTA)){
                     return true;
-
+                // returns false if the inputed tip amount is not a number
                 } else {
                     return false;
                 
                 }
             }
+            // If the inputted bill ammount is not a number, the function returns false
             else {
                 return false;
 
@@ -28,10 +31,19 @@ namespace TipCalculator
                 
         }
         // Function that calculates the final bill total
-        private double calculateFinalBill() {
-            double billAmount = 0;
-
-            return billAmount;
+        private double calculateFinalBill(string billA, string tipA) {
+            double fbillAmount = 0;
+            // Converts the bill ammount to a double and stores it
+            double dBillAmount = Convert.ToDouble(billA);
+            // Converts the tip ammount to a double and stores it
+            double dTipAmount = Convert.ToDouble(tipA);
+            // This is the begining of the calculation Math
+            // Divides the Tip amount by the bill amount to get the total cost of the tip
+            double calTipAmmount = dTipAmount/dBillAmount;
+            // Adds the calulated tip ammount to the bill amount to get the final bill amount
+            fbillAmount = calTipAmmount + dBillAmount;
+            // Returns the Final Bill Amount 
+            return fbillAmount;
         
         
         
@@ -51,7 +63,7 @@ namespace TipCalculator
             if (areInputsValid(inputBillAmount, inputTipAmount) == true)
             {
                 // outputLabel displays the amount calculated by the calculateFinalBill function as a string
-                OutputLabel.Text = calculateFinalBill().ToString();
+                OutputLabel.Text = calculateFinalBill(inputBillAmount, inputTipAmount).ToString();
             }
             else {
                 // Error that displays if areInputsValid comes back as false
--- TipCalculator/TipCalculator/obj/Debug/net8.0-windows/TipCalculator.AssemblyInfo.cs
+++ TipCalculator/TipCalculator/obj/Debug/net8.0-windows/TipCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TipCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0883707308d3fdc818a26f086d2f1cbb3ef5da00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6c47d3f5fc64c68042c18fafd659dfb3682b92")]
 [assembly: System.Reflection.AssemblyProductAttribute("TipCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TipCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
